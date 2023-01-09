e8d5cf030572c973c0118e27c0e88b21c23f5d33
eyndjl <eyndjl@eyndjl.debian>
2023-01-10 00:47:54 +0500

    Переписал README и добавил стандартные модули

---
+++ modules/default/ey_msg.py
@@ -0,0 +1,16 @@
+from modules.api import *
+
+logging.info("Загружен модуль EY_MSG")
+
+@bot.command(name="say", description="Вывод сообщения от имени бота")
+@has_permissions(administrator=True)
+async def say(ctx, text):
+    await ctx.send(text)
+    embed = discord.Embed(title=message_sent, description=" ", colour=0xFFE933)
+    await ctx.response.send_message(embed=embed, ephemeral=True)
+    logging.info(f"Использование команды: /say пользователем {ctx.author.name} с id: {ctx.author.id} | Текст сообщения: \"{text}\"") 
+
+@say.error
+async def say_error(ctx, error):
+    embed = discord.Embed(title=error_text, description=" ", colour=0xFFE933)
+    await ctx.response.send_message(embed=embed, ephemeral=True)
---
+++ modules/default/warn_system.py
@@ -0,0 +1,70 @@
+from modules.api import *
+import sqlite3
+
+logging.info("Загружен модуль WARN_SYSTEM")
+
+#Настройки
+WARNSCOUNT = 3 #Максимальное число предупреждений, при достижении указанного числа выдаётся блокировка
+BLOCKDURATION = 10080 #Длительность блокировки, указывается в минутах
+
+#Создание необходимых папок
+if not os.path.isdir(f"database/servers"):
+    logging.info("Папка \"database/servers\" не найдена. Создание папки...")
+    os.mkdir(f'database')
+    os.mkdir(f'database/servers')
+
+#Система варнов на sqlite3.
+@bot.command(name="warn", description="Выдать предупреждение пользователю")
+@has_permissions(administrator=True)
+async def warn(ctx, member: discord.Member, reason: str):
+    logging.info(f"Использование команды: /warn пользователем {ctx.author.name} с id: {ctx.author.id} | Причина: \"{reason}\"") 
+    serverid = ctx.guild.id
+    if not os.path.isdir(f"database/servers/{serverid}"):
+        os.mkdir(f'database/servers/{serverid}')
+    sqlite_conn = sqlite3.connect(f'database/servers/{serverid}/users.db')
+    cursor = sqlite_conn.cursor()
+    userid = "id" + str(member.id)
+    now = datetime.datetime.now()
+    reason_full = "[" + str(now.day) + "-"+ str(now.month)+ "-" + str(now.year) + " " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + "] " + reason #Приписка даты варна к причине
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS {userid}(reason TEXT);")
+    cursor.execute(f"INSERT INTO {userid} (reason) VALUES('{reason_full}');")
+    sqlite_conn.commit()
+    warns = len(cursor.execute(f"SELECT * FROM {userid}").fetchall())
+    embed = discord.Embed(title=f'Пользователю {member.name} выдано предупреждение.', description=f'Причина: {reason}', colour=0xFFE933)
+    await ctx.response.send_message(embed=embed)
+    if warns >= WARNSCOUNT:
+        logging.info(f"Пользователь {member.name} заблокирован (warns >= {WARNSCOUNT})")
+        embed2 = discord.Embed(title="Пользователю был ограничен доступ к текстовому и голосовому чату на 7 дней за нарушения правил.", description='', colour=0xFFE933)
+        await member.timeout_for(datetime.timedelta(minutes=BLOCKDURATION))
+        await ctx.send(embed=embed2)
+        cursor.execute(f"DELETE FROM {userid}")
+        sqlite_conn.commit()
+    else:
+        pass
+    sqlite_conn.close()
+
+@warn.error
+async def warn_error(ctx, error):
+    embed = discord.Embed(title=error_text, description=" ", colour=0xFFE933)
+    await ctx.response.send_message(embed=embed, ephemeral=True)
+
+@bot.command(name="warns", description="Отобразить список предупреждений пользователя")
+@has_permissions(administrator=True)
+async def warns(ctx, user:discord.User):
+    logging.info(f"Использование команды: /warns пользователем {ctx.author.name} с id: {ctx.author.id}")
+    serverid = ctx.guild.id
+    sqlite_conn = sqlite3.connect(f'database/servers/{serverid}/users.db')
+    cursor = sqlite_conn.cursor()
+    userid = "id" + str(user.id)
+    warns_reasons = cursor.execute(f"SELECT reason FROM {userid}").fetchall()
+    warns = len(warns_reasons)
+    reasons_clean = str(warns_reasons).replace("[(\'", '').replace("(\'", '').replace("\',)", '').replace(",", '\n') #Добавить проверку на количество варнов
+    reasons_form = str(reasons_clean[:len(reasons_clean)-1])
+    embed = discord.Embed(title=f'Предупреждения пользователя {user.name}', description='  ', colour=0xFFE933)
+    embed.add_field(name=f"Количество предупреждений: {warns}, причины:", value=f"{reasons_form}", inline=False)
+    await ctx.response.send_message(embed=embed, ephemeral=True)
+
+@warns.error
+async def warns_error(ctx, error):
+    embed = discord.Embed(title=error_text, description=" ", colour=0xFFE933)
+    await ctx.response.send_message(embed=embed, ephemeral=True)
